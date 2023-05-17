0e5a3633b990ef8bc26662685547b16bab99e792
IvanUlev <126666225+IvanUlev@users.noreply.github.com>
2023-05-17 14:25:58 +0300

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -1,44 +1,46 @@
 import RPi.GPIO as GPIO
-import time as time
-def dec2bin(a):
-    return [int (num) for num in bin(a)[2:].zfill(8)]
-def bin2list(arr, a):
-    for i in range(0,8):
-        if (a & pow(2, i) == pow(2, i)):
-            arr[7 - i] = 1
-        else:
-            arr[7 - i] = 0
-    return arr
+import time
+
+
+def decimal2binary(value):
+    return [int(i) for i in bin(value)[2:].zfill(8)]
+
+
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
+dac    = [26, 19, 13, 6, 5, 11, 9, 10]
+comp   = 4
+troyka = 17
+bits   = len(dac)
+levels = 2 ** bits
+max_voltage = 3.3
+
+
 GPIO.setmode(GPIO.BCM)
-dac = [26, 19, 13, 6, 5, 11, 9, 10]
-arr = [ 0, 0, 0, 0, 0, 0, 0, 0]
 GPIO.setup(dac, GPIO.OUT)
-per = float(input())  
-per = per / 512
-c = 1
-while (c):
-    try: 
-    
-        a = 0  
-        while (1):
-            while(a < 255):
-                a = a+1
-                time.sleep (per)
-                bin2list (arr,a)
-                GPIO.output(dac, arr)
-                print (arr)
-            while (a > 0):
 
-                a = a-1
-                time.sleep (per)
-                bin2list (arr,a)
-                GPIO.output(dac, arr)
-                print(arr)
-          
-    except Err:
-        printf('error')
+try:
+    t = int(input())
+except ValueError:
+    t = 10
 
-    finally:
-        GPIO.output(dac,0)
-        GPIO.cleanup()
-        c = 0
+try:
+    num = 0
+    delta = 1
+
+    while True:
+        GPIO.output(dac, decimal2binary(num))
+        time.sleep(t / 512)
+        if (num + delta) > 255 or (num + delta) < 0:
+            delta = -delta
+        num += delta
+
+finally:
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
