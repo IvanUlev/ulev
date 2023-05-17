248981663761c0bc6bbefb6208576ec3c2a5bae0
IvanUlev <126666225+IvanUlev@users.noreply.github.com>
2023-05-17 14:26:40 +0300

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -1,13 +1,45 @@
 import RPi.GPIO as GPIO
-import time as time
 
-leds = [21, 20, 16, 12, 7, 8, 25, 24]
-aux = [22,23,27, 18, 15, 14 , 3, 2]
+
+def decimal2binary(value):
+    return [int(i) for i in bin(value)[2:].zfill(8)]
+
+
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
+max_voltage = 3.3
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(leds, GPIO.OUT)
-GPIO.setup(aux, GPIO.IN)
-while(1):
-    for i in range(0,8):
-        GPIO.output(leds[i], GPIO.input(aux[i]))
+GPIO.setup(2, GPIO.OUT)
 
-GPIO.cleanup() 
+pwm = GPIO.PWM(2, 100)
+pwm.start(0)
+
+try:
+    while True:
+        koeff = input()
+
+        if koeff == 'q':
+            break
+        
+        if not is_number(koeff):
+            print("Введено не число")
+            continue
+        koeff = float(koeff)
+        if koeff < 0 or koeff > 100:
+            print("Неверное число")
+            continue
+
+        print("Напряжение - {:.3}".format((koeff / 100) * max_voltage))
+        pwm.stop()
+        pwm.start(koeff)
+
+finally:
+    pwm.stop()
+    GPIO.cleanup()
